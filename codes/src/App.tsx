352e1a63ddaa4845b7e35eda2fb094564dbf0816
sunjom <saysaysay223@naver.com>
2024-08-25 14:20:05 +0900

    [FE] refactor : redux사용함으로써 소스코드 변경

--- codes/src/App.tsx
+++ codes/src/App.tsx
@@ -1,10 +1,28 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import Final from './component/MainPage/Final';
+import Header from './component/HeaderList/Header';
+import ErrorOccured from './component/ErrorPage/ErrorOccured';
+import DetailsBoxPage from './component/MainPage/DetailsPage/DetailsBoxPage';
+import { Provider } from 'react-redux';
+import store from './component/reduxData/dataProducer';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Header/>,
+    errorElement: <ErrorOccured/>,
+    children:[
+      {path:'/', element:<Final/>},
+      {path:'/box/:urlId', element:<DetailsBoxPage/>}
+    ]
+  }]);
+
 function App() {
   return (
-    <div className='bg'>
-      <Final/>
-    </div>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   );
 }
 
