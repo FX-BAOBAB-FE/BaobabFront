f45d7dedbfebb6846c909fff6eb0f4a7560da98c
sunjom <saysaysay223@naver.com>
2024-08-25 14:12:48 +0900

    [FE] feat : 주소 잘못입력시 에러 페이지 구현

---
+++ codes/src/component/ErrorPage/ErrorOccured.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ErrorOccured:React.FC<{}> = ()=>{
+    return(
+        <div>
+            <h1>url 잘못 입력함</h1>
+        </div>
+    )
+}
+
+
+export default ErrorOccured;
