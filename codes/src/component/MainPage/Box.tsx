9820c90e5e1e2054962be2f04ffec761d492a1d2
sunjom <saysaysay223@naver.com>
2024-08-25 14:19:36 +0900

    [FE] refactor : router사용으로 구조 변경

--- codes/src/component/MainPage/Box.tsx
+++ codes/src/component/MainPage/Box.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Dummy from '../../Img/dummy.png'
-const Box:React.FC<{}> = ()=>{
+import BoxData from './BoxData';
+const Box:React.FC<{Src:string}> = (props)=>{
     return(
-        <div className='w-full h-[28rem] rounded-xl mt-14 text-center bg-white'>
-            <img className='w-full h-[15rem] object-fill' src={Dummy} alt="dummy"/>
-            <h2 className='mt-6 text-2xl font-bold'>Title</h2> 
-            <p className='text-xl py-3 '>Lorem ipsum dolor sit amet, consect etur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
+            <div className='w-full h-[28rem] rounded-xl mt-14 text-center bg-white'>
+                <img className='w-full h-[15rem] object-fill' src={props.Src} alt="dummy"/>
+                <h2 className='mt-6 text-2xl font-bold'>Title</h2> 
+                <p className='text-xl py-3 '>Lorem ipsum dolor sit amet, consect etur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
     )
 }
 
