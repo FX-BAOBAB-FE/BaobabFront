91ebfe53b7548460bee3c7d039e43b14976aa26d
sunjom <saysaysay223@naver.com>
2024-08-26 10:50:52 +0900

    [FE] feat : 오류 발생시 메뉴바 생성

--- codes/src/component/ErrorPage/ErrorOccured.tsx
+++ codes/src/component/ErrorPage/ErrorOccured.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../HeaderList/Header';
 
 const ErrorOccured:React.FC<{}> = ()=>{
     return(
         <div>
+            <Header/>
             <h1>url 잘못 입력함</h1>
         </div>
     )
