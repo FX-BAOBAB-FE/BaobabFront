992c12de171cd2b83b56135ab6ac4c43bbd385a5
sunjom <saysaysay223@naver.com>
2024-12-27 18:13:09 +0900

    [FE] feat : Redux에 sesstionStorage기능 추가

--- codes/src/component/UsedGoods/DetailsPage/ImgSilder/SildImg.tsx
+++ codes/src/component/UsedGoods/DetailsPage/ImgSilder/SildImg.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import Left from './Img/chevron-left.png';
 import Right from './Img/chevron-right.png';
-import ShowImg from './ShowImg';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import BoxDataObj from '../../../Types/BoxDataObj';
@@ -10,23 +9,24 @@ export default function SildImg(){
     const ImgRef = useRef<HTMLDivElement>(null);
     const [imgAdjust, setImgAdjust] = useState(0);
     const [currentIndex, setCurrentIndex] = useState(0);  // 현재 이미지를 추적하는 상태 변수
-    const [ImgArr, setImgArr] = useState<string[]>([]);
     const {urlId} = useParams();
-    //const storedData = useLoaderData() as string[]
-    const findData = useSelector((state: any) =>
-        state.box.filter((idx:BoxDataObj)=> idx.id == urlId)
-    );
-    const data = findData[0]
-    const storedData = data.img
-    console.log(ImgRef.current)
-    useEffect(()=>{
-        const storage = localStorage.getItem('Imgs');
-        if (storage) {
-            setImgArr(JSON.parse(storage));
-        }
-        
-    },[])
+    const comparId = parseInt(urlId || '0');
     
+    const findData = useSelector((state: any) =>
+        state.box.filter((idx:BoxDataObj)=> idx.id == comparId)
+    );
+    //findData => [[data]] 형식으로 저장되기 때문에 findData[0]으로 둠.
+    const data = findData[0]
+    let storedData:string[] = data.img
+
+    useEffect(()=>{
+        if(!localStorage.getItem('SildImg')){
+            localStorage.setItem("SildImg", data)
+        }else{
+            const a = localStorage.getItem('SildImg')
+            console.log(a);
+        }
+    },[])
     function handlerLeftBtn() {
         if (ImgRef.current && currentIndex > 0) {
             const newIndex = currentIndex - 1;
@@ -36,7 +36,7 @@ export default function SildImg(){
     }
 
     function handlerRightBtn() {
-        if (ImgRef.current && currentIndex < ImgArr.length - 1) {
+        if (ImgRef.current && currentIndex < storedData.length - 1) {
             const newIndex = currentIndex + 1;
             setCurrentIndex(newIndex);
             ImgRef.current.style.transform = `translateX(${-ImgRef.current.offsetWidth * newIndex}px)`;
@@ -71,12 +71,12 @@ export default function SildImg(){
                         src={data} alt="not found box" />)}
                     </div>
                 </div>
-                <button disabled={currentIndex >= ImgArr.length - 1} onClick={handlerRightBtn}>
+                <button disabled={currentIndex >= storedData.length - 1} onClick={handlerRightBtn}>
                     <img src={Right} alt='Right cursor' />
                 </button>
             </div>
             <div className='flex w-[10rem] h-[0.5rem] justify-center gap-x-1 mt-3'>
-                {ImgArr.map((val, index) =>
+                {storedData.map((val, index) =>
                     index === currentIndex ? (
                         <div key={index} className='w-[0.5rem] bg-[#343A40] rounded-full'></div>
                     ) : (
