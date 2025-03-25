2a8c648f920de92e5ab4ef2d5f41a8579748becd
sunjom <saysaysay223@naver.com>
2025-03-25 21:31:16 +0900

    [FE] feat : 중고물품 데이터 호출 후 데이터 연결 완료

--- codes/src/component/UsedGoods/BoxList.tsx
+++ codes/src/component/UsedGoods/BoxList.tsx
@@ -2,29 +2,25 @@ import { useEffect, useRef, useState } from 'react'
 import dummy from '../../Img/dummy.png'
 import Box from './Box';
 import MoreBtn from './MoreBtn';
-import BoxData from './BoxData'
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { boxAction } from '../reduxData/box-Slice';
 import Category from './Category';
 import RegisterBtn from './RegisterBtn';
 import { AllLoad } from '../fetch/articleLoad';
+import BoxDataObj from '../Types/BoxDataObj';
 export default function BoxList(){
-    //data부분은 나중에 서버와 연결하여 axios로 대체할거임
-    let qwe:number = 0;
-    const data:BoxData[] = Array.from(
-        { length: 20 },
-        () => new BoxData(qwe+=1,[dummy,dummy,dummy])
-    );
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        data.map((dat) => {
-            dispatch(boxAction.setBoxData(dat.showData()));})
-    },[data, dispatch]);
+    const data = useLoaderData() as BoxDataObj[];
+    const [nowData,setNowData] = useState<BoxDataObj[]>([]);
 
-    const [nowData,setNowData] = useState(data.slice(0,6));
+    useEffect(()=>{
+        if(data.length > 0){
+            setNowData(data.slice(0,6));
+        }
+    },[data])
+
     const num = useRef(0);
     const [btnCheck,setBtnCheck] = useState(false);
+
     function handlerMoreBtn(){
         if(data.length - nowData.length >=9){
             num.current +=1;
@@ -47,13 +43,21 @@ export default function BoxList(){
             <Category/>
             <RegisterBtn/>
             <div className='w-[80%] h-full grid grid-cols-[repeat(auto-fit,minmax(25rem,1fr))] gap-10 text-center'>
-                {nowData.map(
+                {/* {data.map(
                     (dataObj)=>
-                        {return <Link key={dataObj.id} to={`${dataObj.id}`}><Box Src={dataObj.img[0]} /></Link>}
+                        {return <Link key={dataObj.id} to={`${dataObj.id}`}><Box Src={dataObj.imageList[0].imageUrl} /></Link>}
                     )
-                }
+                } */}
+                {data.map((dataObj) => 
+                    <Link key={dataObj.id} to={`${dataObj.id}`}>
+                        <Box imgs={dataObj.imageList} title={dataObj.title} content={dataObj.content}/>
+                    </Link>)}
             </div>
-            <MoreBtn handlerMoreBtn={handlerMoreBtn} btnCheck={btnCheck}/>
+            {/* <MoreBtn handlerMoreBtn={handlerMoreBtn} btnCheck={btnCheck}/> */}
         </div>
     )
+}
+
+export const Loader = async()=>{
+    return await AllLoad()
 }
--- codes/src/component/fetch/articleLoad.js
+++ codes/src/component/fetch/articleLoad.js
@@ -1,8 +1,8 @@
 import FormDataInstance from "./FormDataInstance";
-
 export const AllLoad = async () =>{
     try{
         const data = await FormDataInstance.get('/article-service/api/list');
+        console.log(data.data.body)
         return data.data.body
     }catch(e){
         console.log(e);
