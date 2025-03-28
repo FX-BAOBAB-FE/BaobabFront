38c4cd677567e3392a91bca815f204e48e0dc824
sunjom <saysaysay223@naver.com>
2025-03-28 13:09:18 +0900

    Rename:CODE

--- codes/src/component/fetch/articleLoad.js
+++ codes/src/component/fetch/articleLoad.js
@@ -2,9 +2,12 @@ import FormDataInstance from "./FormDataInstance";
 export const AllLoad = async () =>{
     try{
         const data = await FormDataInstance.get('/article-service/api/list');
-        console.log(data.data.body)
         return data.data.body
     }catch(e){
         console.log(e);
     }
+}
+
+export const categoryLoad = ()=>{
+    
 }
