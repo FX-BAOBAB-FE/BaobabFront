6bb3ed589ecc7a1b8a9f85cf1f8f0fa1b44ec088
sunjom <saysaysay223@naver.com>
2025-04-07 15:17:51 +0900

    [FE] feat : 수정 버튼 구현

--- codes/src/component/UsedGoods/UserPart/UserCompoent/UserContent.tsx
+++ codes/src/component/UsedGoods/UserPart/UserCompoent/UserContent.tsx
@@ -7,11 +7,11 @@ import BoxDataObj from "../../../Types/BoxDataObj";
 import { deletePoster } from "../../../fetch/articleLoad";
 import { useState } from "react";
 import YesOrNoModal from "../../../Modal/YesOrNoModal";
+import { Button } from "react-bootstrap";
 export default function UserContext(){
     const data = useLoaderData() as BoxDataObj
     const {urlId} = useParams();
     const navigate = useNavigate();
-    console.log(urlId);
 
     const DeleteFunc = async() => {
         await deletePoster(urlId);
@@ -26,14 +26,15 @@ export default function UserContext(){
                 <br/>
                 <p> </p>
             </div>
-            <div className="flex justify-between">
-                <div className="flex">
+            <div className="flex justify-between items-center mb-2">
+                <div className="flex h-[2rem]">
                     <LikeSee img={heart} alt={"하트 이미지"}/>
                     <LikeSee img={message} alt={"메세지 이미지"}/>
                     <LikeSee img={eye} alt={"본횟수 이미지"}/>
                 </div>
-                <div className="w-[5rem] h-[3rem]">
-                    <YesOrNoModal title="삭제" content="정말 삭제하시겠습니까" handleFunc={DeleteFunc}/>
+                <div className="w-[10rem] h-[3rem] flex mb-2">
+                    <Button variant='outline-success' className='w-[5rem] mr-3' onClick={() => navigate('edit')}>수정</Button>
+                    <YesOrNoModal title="삭제" content="정말 삭제하시겠습니까" color="danger" handleFunc={DeleteFunc}/>
                 </div>
             </div>
         </div>
